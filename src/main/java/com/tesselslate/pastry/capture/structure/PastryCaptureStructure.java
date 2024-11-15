31a64aa868f4859de21ceeeaae51a85117ff31e5
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-14 19:21:33 -0500

    feat: add structure API
    The PastryCaptureStructure and PastryCaptureStructurePiece classes wrap
    the data contained within Minecraft's StructureStart and StructurePiece
    classes. These can be written to and read from a Pastry capture file for
    later analysis.

---
+++ src/main/java/com/tesselslate/pastry/capture/structure/PastryCaptureStructure.java
@@ -0,0 +1,56 @@
+package com.tesselslate.pastry.capture.structure;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tesselslate.pastry.capture.PastryCaptureInputStream;
+import com.tesselslate.pastry.capture.PastryCaptureOutputStream;
+
+import net.minecraft.structure.StructurePiece;
+import net.minecraft.structure.StructureStart;
+import net.minecraft.util.math.BlockBox;
+
+/**
+ * Contains information about a single {@link StructureStart} from in the world.
+ */
+public class PastryCaptureStructure {
+    public BlockBox boundingBox;
+    public String name;
+
+    public List<PastryCaptureStructurePiece> pieces;
+
+    public PastryCaptureStructure(StructureStart<?> structureStart) {
+        this.boundingBox = structureStart.getBoundingBox();
+        this.name = structureStart.getFeature().getName();
+
+        this.pieces = new ArrayList<>();
+        for (StructurePiece piece : structureStart.getChildren()) {
+            this.pieces.add(new PastryCaptureStructurePiece(piece));
+        }
+    }
+
+    public PastryCaptureStructure(PastryCaptureInputStream input) throws IOException {
+        this.boundingBox = input.readBlockBox();
+        this.name = input.readString();
+
+        this.pieces = new ArrayList<>();
+
+        int size = input.readInt();
+
+        for (int i = 0; i < size; i++) {
+            this.pieces.add(new PastryCaptureStructurePiece(input));
+        }
+    }
+
+    public void write(PastryCaptureOutputStream output) throws IOException {
+        output.writeBlockBox(this.boundingBox);
+        output.writeString(this.name);
+
+        output.writeInt(this.pieces.size());
+
+        for (PastryCaptureStructurePiece piece : this.pieces) {
+            piece.write(output);
+        }
+    }
+}
---
+++ src/main/java/com/tesselslate/pastry/capture/structure/PastryCaptureStructurePiece.java
@@ -0,0 +1,49 @@
+package com.tesselslate.pastry.capture.structure;
+
+import java.io.IOException;
+
+import com.tesselslate.pastry.capture.PastryCaptureInputStream;
+import com.tesselslate.pastry.capture.PastryCaptureOutputStream;
+
+import net.minecraft.structure.StructurePiece;
+import net.minecraft.structure.StructurePieceType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.Registry;
+
+/**
+ * Contains information about a single {@link StructurePiece} from an in-world
+ * structure.
+ */
+public class PastryCaptureStructurePiece {
+    public StructurePieceType type;
+    public BlockBox boundingBox;
+    public Direction rotation;
+
+    public PastryCaptureStructurePiece(StructurePiece piece) {
+        this.type = piece.getType();
+        this.boundingBox = piece.getBoundingBox();
+        this.rotation = piece.getFacing();
+    }
+
+    /**
+     * @throws IndexOutOfBoundsException If the piece type ID is null
+     */
+    public PastryCaptureStructurePiece(PastryCaptureInputStream input) throws IndexOutOfBoundsException, IOException {
+        String typeId = input.readString();
+        if (typeId == null) {
+            throw new IndexOutOfBoundsException("Cannot create PastryStructurePiece with null type ID");
+        }
+        this.type = Registry.STRUCTURE_PIECE.get(new Identifier(typeId));
+
+        this.boundingBox = input.readBlockBox();
+        this.rotation = Direction.byId(input.readInt());
+    }
+
+    public void write(PastryCaptureOutputStream output) throws IOException {
+        output.writeString(Registry.STRUCTURE_PIECE.getId(this.type).toString());
+        output.writeBlockBox(this.boundingBox);
+        output.writeInt(this.rotation.getId());
+    }
+}

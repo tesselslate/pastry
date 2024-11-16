3a23c29fa2c7351f5530d7f9e8918ee6078ba040
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-16 12:50:07 -0500

    fix: do not use captured frustum for culling
    Unfortunately, there seems to be a weird crash when flying too far
    outside of the range of the captured frustum (and the stack trace is
    completely unhelpful.) So, the captured frustum cannot be used to decide
    which subchunks to render.

---
+++ src/main/java/com/tesselslate/pastry/mixin/cullvis/WorldRendererMixin.java
@@ -0,0 +1,19 @@
+package com.tesselslate.pastry.mixin.cullvis;
+
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.WorldRenderer;
+
+@Mixin(value = WorldRenderer.class)
+public abstract class WorldRendererMixin {
+    @WrapOperation(at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;capturedFrustum:Lnet/minecraft/client/render/Frustum;", opcode = Opcodes.GETFIELD), method = "render(Lnet/minecraft/client/util/math/MatrixStack;FJZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lnet/minecraft/util/math/Matrix4f;)V")
+    private Frustum render_doNotUseCapturedFrustum(WorldRenderer renderer, Operation<Frustum> orig) {
+        return null;
+    }
+}

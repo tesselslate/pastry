77396a45b53186e4e9542274b99ebb7929d17447
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-15 19:17:58 -0500

    fix: stop capture on game close
    Previously, closing the game with an open world would result in the
    capture never being stopped and written to disk.

--- src/main/java/com/tesselslate/pastry/mixin/capture/MinecraftClientMixin.java
+++ src/main/java/com/tesselslate/pastry/mixin/capture/MinecraftClientMixin.java
@@ -2,11 +2,15 @@ package com.tesselslate.pastry.mixin.capture;
 
 import java.util.Objects;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.tesselslate.pastry.Pastry;
 import com.tesselslate.pastry.capture.PastryCaptureEvent;
 import com.tesselslate.pastry.capture.PastryCaptureManager;
 import com.tesselslate.pastry.capture.events.PastryCaptureFrameEvent;
@@ -15,8 +19,6 @@ import com.tesselslate.pastry.capture.events.PastryCaptureWorldLoadEvent;
 import com.tesselslate.pastry.mixin.accessor.WorldRendererAccessor;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ProgressScreen;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -26,6 +28,34 @@ import net.minecraft.util.profiler.ProfileResult;
 
 @Mixin(value = MinecraftClient.class)
 public abstract class MinecraftClientMixin {
+    @Inject(at = @At("HEAD"), method = "joinWorld(Lnet/minecraft/client/world/ClientWorld;)V")
+    private void joinWorld_startCapture(ClientWorld world, CallbackInfo info) {
+        if (PastryCaptureManager.isCapturing()) {
+            return;
+        }
+
+        PastryCaptureManager.startCapture();
+
+        IntegratedServer server = MinecraftClient.getInstance().getServer();
+        if (server != null) {
+            PastryCaptureManager
+                    .update(capture -> capture.add(new PastryCaptureWorldLoadEvent(server.getSaveProperties())));
+        }
+    }
+
+    @WrapOperation(at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;server:Lnet/minecraft/server/integrated/IntegratedServer;", opcode = Opcodes.GETFIELD), method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
+    private IntegratedServer disconnect_stopCapture(MinecraftClient client, Operation<IntegratedServer> orig) {
+        try {
+            if (PastryCaptureManager.isCapturing()) {
+                PastryCaptureManager.stopCapture();
+            }
+        } catch (Exception e) {
+            Pastry.LOGGER.error("Failed to stop capture: " + e);
+        }
+
+        return orig.call(client);
+    }
+
     @Inject(at = @At("HEAD"), method = "drawProfilerResults(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/profiler/ProfileResult;)V")
     private void drawProfilerResults_addFrameEvent(MatrixStack stack, ProfileResult profileResult, CallbackInfo info) {
         MinecraftClient client = MinecraftClient.getInstance();
@@ -53,23 +83,5 @@ public abstract class MinecraftClientMixin {
             event = new PastryCaptureOptionsEvent(client);
             capture.add(event);
         });
-    }
-
-    @Inject(at = @At("HEAD"), method = "joinWorld(Lnet/minecraft/client/world/ClientWorld;)V")
-    private void joinWorld_startCapture(ClientWorld world, CallbackInfo info) {
-        PastryCaptureManager.startCapture();
-
-        IntegratedServer server = MinecraftClient.getInstance().getServer();
-        if (server != null) {
-            PastryCaptureManager
-                    .update(capture -> capture.add(new PastryCaptureWorldLoadEvent(server.getSaveProperties())));
-        }
-    }
-
-    @Inject(at = @At("HEAD"), method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
-    private void disconnect_stopCapture(Screen screen, CallbackInfo info) {
-        if (!(screen instanceof ProgressScreen)) {
-            PastryCaptureManager.stopCapture();
-        }
     }
 }

8ff29f2cbb4825c7b93d74918cde05c24246f7be
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-17 16:04:15 -0500

    refactor: rename result fields in PastryCapturesScreen
    I also fixed a minor visual bug: when the user had 0 captures, the title
    text rendered as "0 capture".

--- src/main/java/com/tesselslate/pastry/gui/PastryCapturesScreen.java
+++ src/main/java/com/tesselslate/pastry/gui/PastryCapturesScreen.java
@@ -20,9 +20,9 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 
 public class PastryCapturesScreen extends ScreenExtended {
-    private ListCapturesTask.Result captureTaskResult;
-    private CompletableFuture<ListCapturesTask.Result> captureTask;
-    private Exception captureTaskError;
+    private ListCapturesTask.Result result;
+    private Exception taskException;
+    private CompletableFuture<ListCapturesTask.Result> task;
 
     private CaptureListWidget captureList;
     private ButtonWidget doneButton;
@@ -31,26 +31,26 @@ public class PastryCapturesScreen extends ScreenExtended {
     public PastryCapturesScreen(Screen parent) {
         super(parent, new LiteralText("Pastry Captures"));
 
-        this.captureTask = CompletableFuture.supplyAsync(ListCapturesTask::run, Pastry.EXECUTOR);
+        this.task = CompletableFuture.supplyAsync(ListCapturesTask::run, Pastry.EXECUTOR);
     }
 
     @Override
     public void onClose() {
         super.onClose();
 
-        if (this.captureTask != null) {
-            this.captureTask.cancel(true);
+        if (this.task != null) {
+            this.task.cancel(true);
         }
     }
 
     @Override
     protected void init() {
-        if (this.captureTaskResult == null) {
+        if (this.result == null) {
             return;
         }
 
         this.captureList = this.addChild(
-                new CaptureListWidget(this.captureTaskResult.entries, this, this.client, this.width, this.height, 32,
+                new CaptureListWidget(this.result.entries, this, this.client, this.width, this.height, 32,
                         this.height - 32, this.captureList != null ? this.captureList.getPage() : 0));
         this.addButton(this.captureList.createNextPageButton(this.width / 2 + 36, this.height - 27, 20, 20));
         this.addButton(this.captureList.createPrevPageButton(this.width / 2 - 56, this.height - 27, 20, 20));
@@ -62,10 +62,10 @@ public class PastryCapturesScreen extends ScreenExtended {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
 
-        if (this.captureTaskError != null) {
-            this.drawCenteredStringWrapping(matrices, "Failed to get captures: " + this.captureTaskError,
+        if (this.taskException != null) {
+            this.drawCenteredStringWrapping(matrices, "Failed to get captures: " + this.taskException,
                     Formatting.RED.getColorValue());
-        } else if (this.captureTask != null) {
+        } else if (this.task != null) {
             this.drawProgressText(matrices, "Loading captures...");
         } else if (this.captureList != null) {
             this.renderWidgets(matrices, mouseX, mouseY, delta);
@@ -76,23 +76,23 @@ public class PastryCapturesScreen extends ScreenExtended {
 
     @Override
     public void tick() {
-        if (this.captureTask == null) {
+        if (this.task == null) {
             return;
         }
 
         ListCapturesTask.Result result;
         try {
-            result = this.captureTask.getNow(null);
+            result = this.task.getNow(null);
             if (result == null) {
                 return;
             }
 
-            this.captureTask = null;
+            this.task = null;
         } catch (Exception e) {
-            this.captureTaskError = e;
+            this.taskException = e;
 
-            this.captureTask.cancel(true);
-            this.captureTask = null;
+            this.task.cancel(true);
+            this.task = null;
             return;
         }
 
@@ -102,13 +102,13 @@ public class PastryCapturesScreen extends ScreenExtended {
     private void processTaskResult(ListCapturesTask.Result result) {
         Collections.sort(result.entries, (a, b) -> b.header.recordedAt.compareTo(a.header.recordedAt));
 
-        this.captureTaskResult = result;
+        this.result = result;
         this.init();
 
-        int entryCount = this.captureTaskResult.entries.size();
-        this.subtitle = new LiteralText(String.format("%d %s (%s)", entryCount, entryCount > 1 ? "captures" : "capture",
-                FileUtils.byteCountToDisplaySize(this.captureTaskResult.entries.stream().map(entry -> entry.size)
-                        .collect(Collectors.summingLong(Long::longValue)))));
+        int entryCount = this.result.entries.size();
+        this.subtitle = new LiteralText(String.format("%d %s (%s)", entryCount,
+                entryCount != 1 ? "captures" : "capture", FileUtils.byteCountToDisplaySize(this.result.entries.stream()
+                        .map(entry -> entry.size).collect(Collectors.summingLong(Long::longValue)))));
 
         if (result.exceptions.size() > 0) {
             this.client.getToastManager().add(new TaskErrorToast(result.exceptions.size()));

a09e84462cee02066e7e15f56a304a3dda875670
tesselslate <46545045+tesselslate@users.noreply.github.com>
2024-11-24 16:06:05 -0500

    change: draw tooltip to left of PastryMenuButton

--- src/main/java/com/tesselslate/pastry/gui/PastryMenuButton.java
+++ src/main/java/com/tesselslate/pastry/gui/PastryMenuButton.java
@@ -4,15 +4,33 @@ import com.tesselslate.pastry.gui.screen.PrepareCaptureListScreen;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.StringRenderable;
 
 public class PastryMenuButton extends ItemButton {
+    private static final String TOOLTIP = "Pastry";
+
     public PastryMenuButton(Screen screen, int x, int y) {
-        super(screen, x, y, Blocks.END_PORTAL_FRAME.asItem(), button -> {
-            MinecraftClient.getInstance().openScreen(new PrepareCaptureListScreen(screen));
-        }, (button, matrices, mouseX, mouseY) -> {
-            screen.renderTooltip(matrices, StringRenderable.plain("Pastry"), mouseX, mouseY);
-        });
+        super(screen, x, y, Blocks.END_PORTAL_FRAME.asItem(), PastryMenuButton::onPress, PastryMenuButton::onTooltip);
+    }
+
+    private static void onPress(ButtonWidget button) {
+        PastryMenuButton instance = (PastryMenuButton) button;
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.openScreen(new PrepareCaptureListScreen(instance.screen));
+    }
+
+    private static void onTooltip(ButtonWidget button, MatrixStack matrices, int mouseX, int mouseY) {
+        PastryMenuButton instance = (PastryMenuButton) button;
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        TextRenderer textRenderer = client.textRenderer;
+
+        instance.screen.renderTooltip(matrices, StringRenderable.plain(TOOLTIP),
+                instance.x - 20 - textRenderer.getWidth(TOOLTIP), instance.y + 22 - (textRenderer.fontHeight / 2));
     }
 }
